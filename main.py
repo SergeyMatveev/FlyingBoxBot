586ab08485825eb95770ff581fa2c9266d19d2e0
Sergey Matveev <matveev.sa@gmail.com>
2023-11-07 17:12:36 +0200

    ver 10 da game is started

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import logging
+
 from telegram.ext import Updater
 from handlers import setup_handlers
 from constants import TOKEN
 
 
 def main():
+    logging.basicConfig(level=logging.INFO)
     updater = Updater(token=TOKEN)
     setup_handlers(updater)
     updater.start_polling()
