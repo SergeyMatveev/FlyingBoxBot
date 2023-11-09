344a740a3fd71104970264e0f0b90d4489801624
Sergey Matveev <matveev.sa@gmail.com>
2023-11-09 16:54:14 +0200

    ver 11 first tickets fixed

--- services/delete.py
+++ services/delete.py
@@ -33,21 +33,8 @@ def kill_that_bitch(update, context):
         context.user_data['conversation'] = False
         return ConversationHandler.END
     else:
-        # Если заказ не найден, запрашиваем номер заказа снова, давая пользователю 2 дополнительные попытки
-        for attempt in range(2, 4):  # Попытки 2 и 3
-            update.message.reply_text(f"Заказ не найден. Введите номер заказа. Попытка {attempt} из 3:")
-            # Ваш код должен включать способ считывания нового сообщения от пользователя здесь
-
-            order_number = update.message.text.strip()  # Предполагаем, что здесь мы снова получаем текст от пользователя
-
-            if mark_order_as_done(username, order_number):
-                update.message.reply_text(f"Заказ номер {order_number} удален.")
-                context.user_data['conversation'] = False
-                return ConversationHandler.END
-
-        # Если до сих пор заказ не найден, информируем пользователя и прерываем процесс
-        update.message.reply_text(f"Вы ввели 3 раза номер заказа, которого мы не можем найти. "
-                                  f"Процесс удаления прерван. \nНапишите в поддержку нажав /help")
+        update.message.reply_text(f"Шаг 2/2. Заказ с таким номером не найден."
+                                  f"Начните процесс заново нажав /delete")
         context.user_data['conversation'] = False
         return ConversationHandler.END
 
