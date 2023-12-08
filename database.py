5736f65c441d5c2550932260d67af5c125e8eccf
Sergey Matveev <matveev.sa@gmail.com>
2023-12-08 20:59:21 +0200

    Release candidate 1

--- database.py
+++ database.py
@@ -202,33 +202,3 @@ def find_matches(order_id):
         return matches
 
 
-def update_completed_orders():
-    """
-    Обновление статуса is_completed в заказах, где текущая дата больше, чем departure_date.
-    """
-    try:
-        with connect_to_database() as conn:
-            with conn.cursor() as cur:
-                # Получаем текущую дату
-                current_date = datetime.now().date()
-
-                # SQL-запрос для обновления статуса
-                update_query = """
-                UPDATE orders
-                SET is_completed = TRUE
-                WHERE departure_date < %s AND is_completed = FALSE
-                """
-
-                # Выполнение запроса
-                cur.execute(update_query, (current_date,))
-
-                # Фиксируем изменения в базе данных
-                conn.commit()
-
-                # Возвращаем количество обновленных записей
-                logging.info(f"Orders were updated because departure_date < current_date. Amount of rows affected: {cur.rowcount}")
-                return cur.rowcount
-
-    except Exception as e:
-        logging.error(f"Error updating orders: {e}")
-        return None
