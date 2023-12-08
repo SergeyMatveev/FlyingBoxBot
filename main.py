56b992eca71a77f71acbc996ab08ab7d9ff369ce
Sergey Matveev <matveev.sa@gmail.com>
2023-12-08 21:21:28 +0200

    Release candidate 1

--- main.py
+++ main.py
@@ -3,15 +3,30 @@ from logging.handlers import SysLogHandler
 from telegram.ext import Updater
 from handlers import setup_handlers
 from constants import TOKEN
+from datetime import datetime
+
+# Настройка формата даты для имени файла лога
+current_date = datetime.now().strftime("%Y_%m_%d")
+log_filename = f"flyingboxbot_log_{current_date}.log"
+
+# Настройка формата сообщений лога
+log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
 # Настройка логирования
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format=log_format)
+
+# Настройка SysLogHandler
 syslog = SysLogHandler()
 syslog.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-syslog.setFormatter(formatter)
+syslog.setFormatter(logging.Formatter(log_format))
 root_logger = logging.getLogger()
 root_logger.addHandler(syslog)
+
+# Настройка FileHandler для логирования в файл с датой в названии
+file_handler = logging.FileHandler(log_filename)
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter(log_format))
+root_logger.addHandler(file_handler)
 
 
 def main():
