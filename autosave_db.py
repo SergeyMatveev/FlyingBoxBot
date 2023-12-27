326b8916e4a5098c94095acdd72ab4f3cb990ed3
Sergey Matveev <matveev.sa@gmail.com>
2023-12-27 13:15:01 +0200

    Release candidate 1

--- autosave_db.py
+++ autosave_db.py
@@ -1,12 +1,17 @@
 import io
 import logging
-
 import pandas as pd
 from googleapiclient.http import MediaIoBaseUpload
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from datetime import datetime
 import psycopg2
+
+# Настройка логирования
+logging.basicConfig(filename='autosave_db.log',
+                    filemode='a',
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
 DATABASE_NAME = 'postgres'
 DATABASE_USER = 'postgres'
@@ -32,29 +37,37 @@ def connect_to_database():
 # Функция для экспорта данных из таблицы в CSV и загрузки на Google Drive
 def export_data_to_csv_and_upload(table_name):
     conn = connect_to_database()
-    df = pd.read_sql(f"SELECT * FROM {table_name}", conn)
-    conn.close()
+    if conn is not None:
+        try:
+            df = pd.read_sql(f"SELECT * FROM {table_name}", conn)
+            current_date = datetime.now().strftime('%Y-%m-%d')
+            filename = f"{table_name}_{current_date}.csv"
 
-    current_date = datetime.now().strftime('%Y-%m-%d')
-    filename = f"{table_name}_{current_date}.csv"
+            csv_buffer = io.BytesIO()
+            df.to_csv(csv_buffer, index=False, encoding='utf-8')
+            csv_buffer.seek(0)
 
-    csv_buffer = io.BytesIO()
-    df.to_csv(csv_buffer, index=False, encoding='utf-8')
-    csv_buffer.seek(0)
+            credentials = service_account.Credentials.from_service_account_file(
+                'sergey_google_account.json',
+                scopes=['https://www.googleapis.com/auth/drive']
+            )
+            service = build('drive', 'v3', credentials=credentials)
 
-    credentials = service_account.Credentials.from_service_account_file(
-        'sergey_google_account.json',
-        scopes=['https://www.googleapis.com/auth/drive']
-    )
-    service = build('drive', 'v3', credentials=credentials)
+            file_metadata = {
+                'name': filename,
+                'mimeType': 'text/csv',
+                'parents': ['18iWsBXfaGjESM5cw4-1vVxcWS6ThzHyz']
+            }
+            media = MediaIoBaseUpload(csv_buffer, mimetype='text/csv', resumable=True)
+            service.files().create(body=file_metadata, media_body=media, fields='id').execute()
 
-    file_metadata = {
-        'name': filename,
-        'mimeType': 'text/csv',
-        'parents': ['18iWsBXfaGjESM5cw4-1vVxcWS6ThzHyz']
-    }
-    media = MediaIoBaseUpload(csv_buffer, mimetype='text/csv', resumable=True)
-    service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+            logging.info(f"Файл {filename} успешно сохранён и загружен на Google Drive")
+        except Exception as e:
+            logging.error(f"Ошибка при работе с {table_name}: {e}")
+        finally:
+            conn.close()
+    else:
+        logging.error(f"Не удалось подключиться к базе данных для {table_name}")
 
 
 # Основная логика скрипта
